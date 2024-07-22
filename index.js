9dd119f02900897f97247701cb641b028d58df11
Julian Nahim <146409963+NahimBY@users.noreply.github.com>
2024-07-22 12:08:50 -0600

    Finished

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const { connectToDatabase } = require('./public/js/connection/db');
 const routes = require('./public/js/connection/routes');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Configuración de CORS
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
+// Servir archivos estáticos desde el directorio 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Servir archivos HTML desde el directorio 'views'
@@ -16,17 +23,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'views', 'index.html'));
-// });
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ type: "*/*" }));
 
 connectToDatabase().then(() => {
   app.use('/api', routes);
 
-  module.exports = app; // Exporta la aplicación para Vercel
+  // Exporta la aplicación para Vercel
+  module.exports = app; 
 }).catch(err => {
   console.error('No se pudo conectar a la base de datos, cerrando la aplicación');
   process.exit(1);
