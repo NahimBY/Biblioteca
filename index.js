e5328ea94e1f2c786e5bb236492fdbd807e9fc3f
Julian Nahim <146409963+NahimBY@users.noreply.github.com>
2024-07-22 01:51:55 -0600

    solved

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { connectToDatabase } = require('./public/js/connection/db');
 const routes = require('./public/js/connection/routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const allowedOrigins = ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://biblioteca-livid.vercel.app/'];
-
+// Configuración de CORS
+app.use(cors({
+  origin: 'https://biblioteca-livid.vercel.app/', // Cambia esto al dominio correcto
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ type: "*/*" }));
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
-
 
 connectToDatabase().then(() => {
   app.use('/api', routes);
 
-  app.listen(port, () => {
-    console.log(`Está ejecutándose en http://localhost:${port}`);
-  });
+  module.exports = app; // Exporta la aplicación para Vercel
 }).catch(err => {
   console.error('No se pudo conectar a la base de datos, cerrando la aplicación');
   process.exit(1);
